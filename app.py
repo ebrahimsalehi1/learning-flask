dab2e1488a9cf85cafb08c8e10fc701c3c678f54
eeeee.bbbbb.iiiii@gmail.com <eeeee.bbbbb.iiiii@gmail.com>
2022-06-26 17:03:54 +0200

    render_template and its parameters, routes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 
+from ast import operator
 from distutils.log import debug
 from flask import Flask
 from flask import render_template
@@ -20,6 +21,27 @@ def employee():
 @app.route("/isemployee")
 def is_employe():
     return render_template('index.html')
+
+@app.route('/employeeList')    
+def employeeList():
+    from make_data import get_data
+    return render_template('index.html',name='qwqw',empList= map(lambda i:i["salary"],get_data()))
+
+@app.route('/user/<string:userName>/<int:password>')
+def get_user(userName,password):
+    return f"user: {userName} - {password}"
+
+@app.route('/calculate/<string:operation>/<float:n1>/<float:n2>')
+def calculate(operation,n1,n2):
+    res=0
+    if operation=='add':
+        res=n1+n2
+                
+    return f"calc - {operation} - {res}"
+
+@app.route('/path/<path:sub_path>')
+def get_path(sub_path):
+    return f"path: {sub_path}"
 
 if __name__=="__main__":
     app.run(debug=True)
---
+++ make_data.py
@@ -0,0 +1,14 @@
+
+def get_data():
+    arr = [
+        {
+        "firstName":"ebrahim",
+        "salary":1000
+    },
+    {
+        "firstName":"mahsa",
+        "salary":2000
+    }
+    ]
+
+    return arr
