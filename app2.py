98aa036c91a7d05fc1617ddba94bdd34070aabc4
eeeee.bbbbb.iiiii@gmail.com <eeeee.bbbbb.iiiii@gmail.com>
2022-08-01 18:33:01 +0200

    add some web services for checking the last services

--- app2.py
+++ app2.py
@@ -1,5 +1,4 @@
-from enum import unique
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,14 +6,6 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///mydb.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-    #   "userId": 100,
-    #   "username": "ebrahim2000",
-    #   "password": "1234",
-    #   "firstName": "ebrahim",
-    #   "lastName": "salehi",
-    #   "userType": "teacher"
-
 class User(db.Model):
     id=db.Column("user_id",db.Integer,primary_key=True)
     user_name = db.Column(db.String(50),unique=True)
@@ -23,8 +14,6 @@ class User(db.Model):
     last_name = db.Column(db.String(50),nullable=False)
     user_type = db.Column(db.String(50),nullable=False)
 
-
-
     def __init__(self,user_name,password,first_name,last_name,user_type):
         self.user_name=user_name
         self.password=password
@@ -32,27 +21,94 @@ class User(db.Model):
         self.last_name=last_name
         self.user_type=user_type
 
+    def get_json(self):
+        return {
+        "user_name":self.user_name,
+        # "password":self.password,
+        "first_name":self.first_name,
+        "last_name":self.last_name,
+        "user_type":self.user_type
+        }
 
 @app.route('/adduser',methods=['POST'])
 def add_user():
     try:
         body = request.get_json()
 
-        user_name=body['username']
-        password=body['password']
-        first_name=body['firstName']
-        last_name=body['lastName']
-        user_type=body['userType']
+        user_obj = {
+            "user_name":body['username'],
+            "password" :body['password'],
+            "first_name":body['firstName'],
+            "last_name":body['lastName'],
+            "user_type":body['userType']
+        }
 
-        new_user = User(user_name,password,first_name,last_name,user_type)
+        new_user = User(user_obj['user_name'],user_obj['password'],user_obj['first_name'],user_obj['last_name'],user_obj['user_type'])
+        print(new_user.id)
         db.session.add(new_user)
         db.session.commit()
 
-        return "True"
+        return user_obj,200
 
     except Exception as ex:
         print(ex)
-        return "False"
+        codes = 100
+
+        return {"message":str(ex)},417
+
+@app.route('/getusers')
+def get_users():
+    all_users = User.query.all()
+    str_result ='['
+    i=0
+    for user in all_users:
+        str_result = str_result+str(user.get_json())
+        if i<len(all_users)-1:
+            str_result=str_result+","
+
+        i=i+1
+
+    str_result=str_result+']'
+
+    return jsonify(str_result)
+
+
+@app.route('/edituser',methods=['PUT'])
+def edit_user():
+    try:
+        body = request.get_json()
+
+        user_obj = {
+            "user_name":body['username'],
+            "password" :body['password'],
+            "first_name":body['firstName'],
+            "last_name":body['lastName'],
+            "user_type":body['userType']
+        }
+
+        new_user = User(user_obj['user_name'],user_obj['password'],user_obj['first_name'],user_obj['last_name'],user_obj['user_type'])        
+        db.session.commit()
+
+        return user_obj,200
+
+    except Exception as ex:
+        print(ex)
+        codes = 100
+
+        return {"message":str(ex)},417
+
+@app.route('/deleteuser/<user_id>',methods=['DELETE'])
+def delete_user(user_id):
+    try:
+        found_user = User.query.filter_by(id=user_id).first()
+        db.session.delete(found_user)
+        db.session.commit()
+
+        return "OK",200
+
+    except Exception as ex:
+        print(ex)
+        return {"message":str(ex)},404
 
 if __name__=='__main__':
     db.create_all()
